94cab5908cbdaf8f73a934eb0992d5d18f55fab0
Sergey Yermochenko <yermochenko@gmail.com>
2024-05-27 16:33:03 +0300

    FR3 и FR4 завершены

--- src/main/java/by/vsu/twoa/domain/Task.java
+++ src/main/java/by/vsu/twoa/domain/Task.java
@@ -1,11 +1,22 @@
 package by.vsu.twoa.domain;
 
+import by.vsu.twoa.geometry.Circle;
+import by.vsu.twoa.geometry.Point;
+import by.vsu.twoa.geometry.Rectangle;
+
 import java.util.Date;
 
 public class Task extends Entity {
 	private User owner;
 	private String name;
 	private Date created;
+	private Point vertex1;
+	private Point vertex2;
+	private Double radius;
+	private Rectangle rectangleVariant1;
+	private Circle circumCircleVariant1;
+	private Rectangle rectangleVariant2;
+	private Circle circumCircleVariant2;
 
 	public User getOwner() {
 		return owner;
@@ -29,5 +40,61 @@ public class Task extends Entity {
 
 	public void setCreated(Date created) {
 		this.created = created;
+	}
+
+	public Point getVertex1() {
+		return vertex1;
+	}
+
+	public void setVertex1(Point vertex1) {
+		this.vertex1 = vertex1;
+	}
+
+	public Point getVertex2() {
+		return vertex2;
+	}
+
+	public void setVertex2(Point vertex2) {
+		this.vertex2 = vertex2;
+	}
+
+	public Double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+
+	public Rectangle getRectangleVariant1() {
+		return rectangleVariant1;
+	}
+
+	public void setRectangleVariant1(Rectangle rectangleVariant1) {
+		this.rectangleVariant1 = rectangleVariant1;
+	}
+
+	public Circle getCircumCircleVariant1() {
+		return circumCircleVariant1;
+	}
+
+	public void setCircumCircleVariant1(Circle circumCircleVariant1) {
+		this.circumCircleVariant1 = circumCircleVariant1;
+	}
+
+	public Rectangle getRectangleVariant2() {
+		return rectangleVariant2;
+	}
+
+	public void setRectangleVariant2(Rectangle rectangleVariant2) {
+		this.rectangleVariant2 = rectangleVariant2;
+	}
+
+	public Circle getCircumCircleVariant2() {
+		return circumCircleVariant2;
+	}
+
+	public void setCircumCircleVariant2(Circle circumCircleVariant2) {
+		this.circumCircleVariant2 = circumCircleVariant2;
 	}
 }
--- src/main/java/by/vsu/twoa/service/TaskService.java
+++ src/main/java/by/vsu/twoa/service/TaskService.java
@@ -5,9 +5,12 @@ import by.vsu.twoa.dao.TaskDao;
 import by.vsu.twoa.dao.UserDao;
 import by.vsu.twoa.domain.Task;
 import by.vsu.twoa.domain.User;
+import by.vsu.twoa.geometry.*;
 import by.vsu.twoa.service.exception.ServiceException;
+import by.vsu.twoa.service.exception.TaskNotExistsException;
 import by.vsu.twoa.service.exception.UserNotExistsException;
 
+import java.util.Date;
 import java.util.List;
 
 public class TaskService {
@@ -28,6 +31,43 @@ public class TaskService {
 			List<Task> tasks = taskDao.readByOwner(id);
 			tasks.forEach(task -> task.setOwner(owner));
 			return tasks;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Task findById(Integer id) throws ServiceException {
+		try {
+			Task task = taskDao.read(id).orElseThrow(() -> new TaskNotExistsException(id));
+			task.setOwner(userDao.read(task.getOwner().getId()).orElseThrow(() -> new UserNotExistsException(id)));
+			Point vertex1 = task.getVertex1();
+			Point vertex2 = task.getVertex2();
+			double radius = task.getRadius();
+			Point middle = new Segment(vertex1, vertex2).middle();
+			Vector v = new Vector(middle, vertex1).rotate(90);
+			double c = radius / v.length();
+			v.multiply(Math.sqrt(c * c - 1));
+			Vector v2 = v.multiply(2);
+			task.setRectangleVariant1(new Rectangle(vertex1, vertex2, v2.put(vertex2), v2.put(vertex1)));
+			task.setCircumCircleVariant1(new Circle(v.put(middle), radius));
+			v = v.multiply(-1);
+			v2 = v2.multiply(-1);
+			task.setRectangleVariant2(new Rectangle(vertex1, vertex2, v2.put(vertex2), v2.put(vertex1)));
+			task.setCircumCircleVariant2(new Circle(v.put(middle), radius));
+			return task;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Integer save(Task task) throws ServiceException {
+		try {
+			if(task.getId() == null) {
+				task.setCreated(new Date(0));
+				return taskDao.create(task);
+			} else {
+				throw new RuntimeException("Update operation not implemented yet");
+			}
 		} catch(DaoException e) {
 			throw new ServiceException(e);
 		}
